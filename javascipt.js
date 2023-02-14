46d8c340bbc2782f6d7a462e12d07a19613bd806
Maxim <polyakovmaxim05@gmail.com>
2023-02-14 16:15:46 -0500

    init

--- javascipt.js
+++ javascipt.js
@@ -82,3 +82,11 @@ var twoSum = function(nums, target) {
       }
   };
 //Two Sum
+
+//Add Binary
+var addBinary = function(a, b) {
+    const aInt = BigInt(`0b${a}`)
+    const bInt = BigInt(`0b${b}`)
+    return (aInt + bInt).toString(2)
+};
+//Add Binary
