8c05dddb5685de90cdc48ea0c0f99309bcbf6144
Maxim <polyakovmaxim05@gmail.com>
2023-02-24 18:51:34 -0500

    init

--- javascipt.js
+++ javascipt.js
@@ -210,3 +210,18 @@ var containsDuplicate = function(nums) {
 }
 //Contains Duplicate
 
+//Find Pivot Index
+var pivotIndex = function(nums) {
+    let leftSum = 0;
+    let rightSum = nums.reduce((a, b) => a + b)
+    for(let i = 0; i < nums.length; i++){
+        rightSum -= nums[i]
+        if(leftSum === rightSum){
+            return i
+        }
+        leftSum += nums[i]
+    }
+    return -1
+};
+//Find Pivot Index
+
