11ca387f7561071f3412f8bb6fbc34644a3c1b15
Maxim <polyakovmaxim05@gmail.com>
2023-02-28 20:42:04 -0500

    init

--- javascipt.js
+++ javascipt.js
@@ -322,3 +322,18 @@ var isValid = function(s) {
 };
 //Valid Parenthesis
 
+
+//Plus One
+var plusOne = function(digits) {
+    for(let i = digits.length - 1; i >= 0; i--){
+        digits[i]++
+        if(digits[i] > 9){
+            digits[i] = 0
+        }else{
+            return digits
+        }
+    }
+    digits.unshift(1)
+    return digits;
+};
+//Plus One
